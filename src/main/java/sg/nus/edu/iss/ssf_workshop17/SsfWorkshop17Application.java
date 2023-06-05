0f1a75e2a56c258f5024efbea6216f60277f786d
russell258 <duckie258@gmail.com>
2023-06-05 12:11:45 +0800

    first setup

---
+++ src/main/java/sg/nus/edu/iss/ssf_workshop17/SsfWorkshop17Application.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.ssf_workshop17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfWorkshop17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfWorkshop17Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/nus/edu/iss/ssf_workshop17/SsfWorkshop17ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.ssf_workshop17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfWorkshop17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
